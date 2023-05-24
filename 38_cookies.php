2cfc7e64905b07771540b2b5d9e432528e9dfbfc
Iamam786 <93914102+Iamam786@users.noreply.github.com>
2023-05-25 00:02:41 +0530

    cookie

---
+++ 38_cookies.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "welcome to the world of cookies<br>";
+//Cookies | Sessions
+
+//Syntax to set a cookie
+setcookie("category","Books",time() + 86400,"/");
+echo "the cookie is set<br>";
+
+?>
